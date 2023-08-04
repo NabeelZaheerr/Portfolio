352754d16b17ab7b9df1ce4ef1aeac36a3c0740d
Nabeel Zaheer <128033732+NabeelZaheerr@users.noreply.github.com>
2023-08-04 23:55:11 +0500

    save this change

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desbook pile";
+import image from "../images/book pile.jpg";
 const imageAltText = "table with books";
 
 /**
