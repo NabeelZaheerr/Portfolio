177ddc4b4ae46c69be3539a82e30dba28189c66a
Nabeel Zaheer <128033732+NabeelZaheerr@users.noreply.github.com>
2023-08-04 22:39:14 +0500

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Nabeel Zaheer",
+  title: "3D Modeler and Animator",
+  email: "nabeelzaheeredu@gmail.com",
+  gitHub: "https://github.com/NabeelZaheerricrosoft",
+  linkedIn: "https://www.linkedin.com/in/nabeel-zaheer",
 };
 
 const primaryColor = "#4E567E";
